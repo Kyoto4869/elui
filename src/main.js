dc6405954b8b2766372f4667f9a1a5b21350849c
TL-20191123VLYG\TongLe <1919777283@qq.com>
2020-09-22 08:28:55 +0800

    完成了登录页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import './assets/js/mock.js'
+import './assets/css/reset.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+Vue.prototype.$http=axios;//所有vue的实例都有一个，可以让在任何组件调用
+
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/user/admin/login.vue'
+import User  from '../views/user/index.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
+  
   {
     path: '/',
-    name: 'Home',
-    component: Home
+  	redirect:'/login'
+  },
+  //默认跳到login
+  {
+    path: '/login',
+  	name: 'Login',
+  	component:Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/user',
+  	name: 'User',
+  	component:User
+  },
+  
+  
+  
 ]
 
 const router = new VueRouter({
